887047f5abcf556368da080a194e13d1c22c34bb
William <william.j.schober.reyes@gmail.com>
2019-01-17 16:18:13 -0500

    CRUD Template

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import Create from './components/create.component';
+import Edit from './components/edit.component';
+import Index from './components/index.component';
+
+//https://appdividend.com/2018/11/11/react-crud-example-mern-stack-tutorial/
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to={'/'} className="navbar-brand">React CRUD Example</Link>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                  <Link to={'/'} className="nav-link">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={'/create'} className="nav-link">Create</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={'/index'} className="nav-link">Index</Link>
+                </li>
+              </ul>
+            </div>
+          </nav> <br/>
+          <h2>Welcome to React CRUD Tutorial</h2> <br/>
+          <Switch>
+              <Route exact path='/create' component={ Create } />
+              <Route path='/edit/:id' component={ Edit } />
+              <Route path='/index' component={ Index } />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
